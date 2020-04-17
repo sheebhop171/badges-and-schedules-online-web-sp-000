0b7eba164e92978fa8213cdb64c2a9ce667e9339
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-17 19:24:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+    badges << "Hello, my name is #{name}."
+  end
+    badges
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  count = 1
+  attendees.each do |name|
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{count}!"
+    count += 1
+  end
+  room_assignments
+end
+
+def printer(attendees)
+  puts badges.each do |attendees|
+  end
+  puts room_assignments.each do |rooms|
+  end
+end
